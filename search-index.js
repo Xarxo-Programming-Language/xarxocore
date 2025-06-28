8a37b8fbee80d78bb2e64a0d428eb357a00eea06
alexeev-prog <alexeev-prog@users.noreply.github.com>
2025-06-28 21:43:36 +0000

    deploy: f65b055e0c0cdad240fbb8b44f699924d7054d9f

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = new Map(JSON.parse('[["xarxocore",{"t":"H","n":["main"],"q":[[0,"xarxocore"]],"i":"`","f":"{{}b}","D":"`","p":[[1,"unit"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA=","P":[]}]]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[178]}
